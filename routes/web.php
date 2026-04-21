f925788d4c65a55dfebd3e477bd5661170bb57e8
kentxzy <k.rato.555323@umindanao.edu.ph>
2026-04-21 09:41:19 +0800

    Fixing port and build step

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect('/products');
+});
+
+Route::get('/', function () {
     return view('welcome');
 });
 
